5db88b1e3dc624a3d268765548ed54d81a396da1
NyleVII <stephenantymis@gmail.com>
2019-02-14 18:10:06 -0700

    Initial commit

---
+++ calc.js
@@ -0,0 +1,43 @@
+
+let displayStr = "0"
+let prevCalcStr = "0"
+
+
+function getResult(){
+    prevCalcStr = displayStr + "=";
+
+    try {
+        displayStr = math.eval(displayStr);
+    }
+    catch (e){
+        displayStr = "ERROR";
+    }
+
+    document.getElementById("prevCalc").innerHTML = prevCalcStr;
+    document.getElementById("display").innerHTML = displayStr;
+}
+
+
+
+function updateDisplay(str){
+    // CLEAR button functionality
+    if(str === ""){
+        displayStr = "0"
+    }
+    // Replace the value
+    if(displayStr === "0" && Number(str)){
+        displayStr = str;
+    }
+    else if(displayStr === "ERROR"){
+        displayStr = str;
+    }
+    else{
+        displayStr = displayStr + str;
+    }
+    
+
+    document.getElementById("display").innerHTML = displayStr;
+    console.debug("Update Display: " + str);
+}
+
+
